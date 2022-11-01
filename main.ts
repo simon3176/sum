be6aed1a8ffd657e4e9908ffbc93b08e2eb43981
Nomis3167 <113910266+simon3176@users.noreply.github.com>
2022-11-01 09:17:10 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 9; index++) {
+        Factorials = Factorials + (index + 1)
+    }
+    basic.showNumber(Factorials)
+})
+let Factorials = 0
+Factorials = 0
+basic.forever(function () {
+	
+})
